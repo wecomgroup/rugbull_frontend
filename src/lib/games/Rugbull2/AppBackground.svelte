272bcc1c1d9848da96fe7c6df19783048f2795b3
Nann Ly <nann.ly@1bit.com.ph>
2024-05-24 09:54:42 +0700

    Up

--- src/lib/games/Rugbull2/AppBackground.svelte
+++ src/lib/games/Rugbull2/AppBackground.svelte
@@ -1,1 +1,28 @@
-AppBackground
+<script lang="js">
+  import {spring} from 'svelte/motion';
+  import {onMount} from "svelte";
+  import ScrollableImage from "$lib/games/Rugbull2/components/ScrollableImage.svelte";
+
+  const backgroundX = spring(0, {
+    stiffness: 0.1,
+  });
+
+  onMount(() => {
+    backgroundX.set(-500);
+
+    function mouseMove(e) {
+      backgroundX.set(-e.clientX * 4);
+    }
+
+    window.addEventListener('mousemove', mouseMove);
+  })
+
+</script>
+
+<!--Crater -->
+<ScrollableImage x={$backgroundX}
+              height="100px"
+              src="/images/rugbull2/crater.webp"
+/>
+
+<!--Star background -->
---
+++ src/lib/games/Rugbull2/components/ScrollableImage.svelte
@@ -0,0 +1,35 @@
+<script lang="js">
+  export let x = 0;
+  export let height = "50px";
+  export let src = "/images/rugbull2/crater.webp"
+
+  let totalW;
+
+  $: adjustedX = Math.abs(x % (totalW / 3)) + (totalW / 3);
+</script>
+
+<div class="scrollable-image"
+     style="height:{height}"
+>
+  <div class="images-wrapper" style="transform: translate(-{adjustedX}px, 0); height:{height}" bind:clientWidth={totalW}>
+    <img alt="crater" {src} />
+    <img alt="crater" {src} style="margin-left:-1px"/>
+    <img alt="crater" {src} style="margin-left:-2px"/>
+  </div>
+</div>
+
+
+<style>
+  .scrollable-image {
+    display: flex;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .images-wrapper {
+    position: absolute;
+    left: 0;
+    display: flex;
+  }
+</style>
+
